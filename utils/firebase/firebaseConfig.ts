ad257b298945f1a2f668f66280291b7977a0542b
FlyingTurkman <99084104+FlyingTurkman@users.noreply.github.com>
2023-10-02 01:24:09 +0300

    master

--- utils/firebase/firebaseConfig.ts
+++ utils/firebase/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRq-j6YHcIwugtDKBP524tTv8T77rGz6o",
-  authDomain: "diyetas-c1ad4.firebaseapp.com",
-  projectId: "diyetas-c1ad4",
-  storageBucket: "diyetas-c1ad4.appspot.com",
-  messagingSenderId: "1094216577942",
-  appId: "1:1094216577942:web:66df89b3631129a2fce3b7",
-  measurementId: "G-RXKR3FF12P"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 

f755938f2d6dfa35d2496b68007575383314f3b0
FlyingTurkman <99084104+FlyingTurkman@users.noreply.github.com>
2023-10-17 05:24:46 +0300

    master

--- components/lobby/LobbyDraft.tsx
+++ components/lobby/LobbyDraft.tsx
@@ -10,9 +10,11 @@ import { positionLists } from "@/utils/src/positionLists"
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "../ui/hover-card"
 import { sendMessage } from "./sendMessage"
 import { Input } from "../ui/input"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { io } from "socket.io-client"
 import { lobbyMessages } from "@/utils/src/constants"
+import { VList } from "virtua"
+import { AiFillCrown } from "react-icons/ai"
 
 
 
@@ -23,6 +25,7 @@ const socket = io(`${process.env.socketPath}:${process.env.socketPort}`)
 
 
 export default function LobbyDraft({ lobby, initialMessages , players, userId, token }: { lobby: lobbyType | null | undefined, initialMessages: lobbyMessageType[], players: userType[], userId?: string, token?: string }) {
+    const formRef = useRef<HTMLFormElement>(null)
     const sendMessageWithUserId = sendMessage.bind(null, { userId, lobbyId: lobby?._id.toString()})
     const [messages, setMessages] = useState<lobbyMessageType[]>(initialMessages)
     useEffect(() => {
@@ -55,13 +58,13 @@ export default function LobbyDraft({ lobby, initialMessages , players, userId, t
             <CardContent className="flex flex-col gap-1">
                 <CardDescription>Players waiting for pick</CardDescription>
                 <div className="flex flex-row flex-wrap gap-2">
-                    {lobby?.players.map((p) => {
+                    {lobby?.players.filter((p) => !lobby.homeTeam?.includes(p.playerId) && !lobby.awayTeam?.includes(p.playerId)).map((p) => {
                         const player = players.find((user) => user._id.toString() == p.playerId)
                         if (player) {
                             return(
                                 <HoverCard key={`playerPickButton${player._id.toString()}`}>
                                     <HoverCardTrigger>
-                                        <Button  className="flex flex-row h-full justify-between">
+                                        <Button variant={'green'} className="flex flex-row h-full justify-between" disabled={lobby.turn != userId? true: false}>
                                             {player.username}
                                         </Button>
                                     </HoverCardTrigger>
@@ -96,34 +99,101 @@ export default function LobbyDraft({ lobby, initialMessages , players, userId, t
                 <div className="flex flex-col basis-1/2 gap-2 items-center border-gray-300 border rounded p-2">
                     <CardTitle>Home Team</CardTitle>
                     <hr className="w-full"/>
+                    {lobby?.homeTeam?.map((p, i) => {
+                        const player = players.find((user) => user._id.toString() == p)
+                        return(
+                            <HoverCard key={`playerHomeTeam${player?._id.toString()}`}>
+                                <HoverCardTrigger>
+                                    <Button variant={'red'} className="flex flex-row h-full justify-between" disabled={lobby.turn != userId? true: false}>
+                                        {i == 0? (
+                                            <AiFillCrown/>
+                                        ): null }
+                                        {player?.username?? 'User Not Found'}
+                                    </Button>
+                                </HoverCardTrigger>
+                                <HoverCardContent className="flex flex-row gap-2 items-center">
+                                    <Avatar>
+                                        {player?.avatar && (
+                                            <AvatarImage src={`${process.env.storagePath}/${userAvatar(player._id.toString(), player?.avatar?? '')}`}></AvatarImage>
+                                        )}
+                                        <AvatarFallback className="bg-blue-600 text-white">
+                                            <IoPerson/>
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    {player?.username}
+                                    <div className="flex flex-col gap-1">
+                                        <Badge>
+                                            {positionLists.find((pos) => pos.id == player?.mainPosition)?.position}
+                                        </Badge>
+                                        <Badge>
+                                            {positionLists.find((pos) => pos.id == player?.sidePosition)?.position}
+                                        </Badge>
+                                    </div>
+                                </HoverCardContent>
+                            </HoverCard> 
+                        )
+                    })}
                 </div>
                 <div className="flex flex-col basis-1/2 gap-2 items-center border-gray-300 border rounded p-2">
                     <CardTitle>Away Team</CardTitle>
                     <hr className="w-full"></hr>
+                    {lobby?.awayTeam?.map((p, i) => {
+                        const player = players.find((user) => user._id.toString() == p)
+                        return(
+                            <HoverCard key={`playerAwayTeam${player?._id.toString()}`}>
+                                <HoverCardTrigger>
+                                    <Button className="flex flex-row h-full justify-between" disabled={lobby.turn != userId? true: false}>
+                                        {i == 0? (
+                                            <AiFillCrown/>
+                                        ): null }
+                                        {player?.username?? 'User Not Found'}
+                                    </Button>
+                                </HoverCardTrigger>
+                                <HoverCardContent className="flex flex-row gap-2 items-center">
+                                    <Avatar>
+                                        {player?.avatar && (
+                                            <AvatarImage src={`${process.env.storagePath}/${userAvatar(player._id.toString(), player?.avatar?? '')}`}></AvatarImage>
+                                        )}
+                                        <AvatarFallback className="bg-blue-600 text-white">
+                                            <IoPerson/>
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    {player?.username}
+                                    <div className="flex flex-col gap-1">
+                                        <Badge>
+                                            {positionLists.find((pos) => pos.id == player?.mainPosition)?.position}
+                                        </Badge>
+                                        <Badge>
+                                            {positionLists.find((pos) => pos.id == player?.sidePosition)?.position}
+                                        </Badge>
+                                    </div>
+                                </HoverCardContent>
+                            </HoverCard> 
+                        )
+                    })}
                 </div>
             </CardContent>
             <hr/>
             <CardContent>
-                <form className="flex flex-col gap-2" action={sendMessageWithUserId} autoComplete="off">
+                <form ref={formRef} className="flex flex-col gap-2" action={sendMessageWithUserId} autoComplete="off">
                     <CardDescription>Lobby Chat</CardDescription>
-                    <div id="messagesArea" className="flex flex-col gap-2 p-2 h-60 max-h-60 overflow-auto scrollBar border border-gray-300 rounded">
+                    <VList id="messagesArea" className="flex flex-col gap-2 p-2 h-60 max-h-60 overflow-auto scrollBar border border-gray-300 rounded" style={{height: '240px'}}>
                         {messages.sort((a, b) => new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime()).map((message) => {
                             const player = players.find((p) => p._id.toString() == message.playerId)
                             return(
                                 <div key={`lobbyMessage${message._id.toString()}`} className="flex flex-row items-start gap-1">
                                     <div className="flex flex-col">
-                                        <label className={`font-semibold`}>{player?.username}: </label>
+                                        <label className={`font-semibold ${lobby?.homeTeam?.includes(message.playerId)? 'text-red-600': lobby?.awayTeam?.includes(message.playerId)? 'text-blue-600': 'text-black'} transition-all`}>{player?.username}: </label>
                                         <CardDescription>{new Date(message.dateTime).toLocaleTimeString()}</CardDescription>
                                     </div>
                                     <p>{message.content}</p>
                                 </div>
                             )
                         })}
-                        
-                    </div>
+                    </VList>
                     <hr/>
                     <div className="flex flex-row gap-2">
-                        <Input type="text" placeholder="Message" name="message"/>
+                        <Input type="text" placeholder="Message" id="message" name="message" required/>
                         <Button type="submit" size={'icon'}>
                             <IoSend/>
                         </Button>
